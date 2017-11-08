9894d8cc1ea97a6be974f83e14226b9cd57d2d54
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-07 21:47:04 -0600

    attemps to add an event listener

--- etch-sketch.js
+++ etch-sketch.js
@@ -3,3 +3,11 @@ const board = document.querySelector("#board")
 for(var i = 0; i < 8*8; i++) {
   board.appendChild(document.createElement("DIV"));
 }
+
+for(var i = 0; i < board.children.length; i++) {
+  board.children[i].addEventListener("click", changeColor(board.children[i]));
+}
+
+function changeColor(node) {
+  console.log(node)
+}
