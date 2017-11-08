e6ec699b3d2d4e00e7ceb3f945164770e09f0b45
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-07 18:43:23 -0600

    Creates Grid

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const board = document.querySelector("div#board")
+
+
+for(var i = 0; i < 8; i++) {
+  board.appendChild(document.createElement("DIV"));
+}
+
+board.childNodes.forEach(function(child) {
+  if(child.nodeType === Node.ELEMENT_NODE) {
+    for(var i = 0; i < 8; i++) {
+    child.appendChild(document.createElement("DIV"));
+    }
+  }
+});
