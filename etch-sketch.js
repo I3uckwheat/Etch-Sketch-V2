b7a230b2b733b6c98f54aeb6288ae99d2846d5ec
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-07 21:01:21 -0600

    refactor grid generation

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,14 +1,5 @@
 const board = document.querySelector("div#board")
 
-
-for(var i = 0; i < 8; i++) {
+for(var i = 0; i < 8*8; i++) {
   board.appendChild(document.createElement("DIV"));
 }
-
-board.childNodes.forEach(function(child) {
-  if(child.nodeType === Node.ELEMENT_NODE) {
-    for(var i = 0; i < 8; i++) {
-    child.appendChild(document.createElement("DIV"));
-    }
-  }
-});
