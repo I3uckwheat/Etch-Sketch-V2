bafe34e164bdb78f40e0ec3120e75cf345ac68d5
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-08 21:14:00 -0600

    refactor and adds button

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,13 +1,28 @@
 const board = document.querySelector("#board")
 
-for(var i = 0; i < 8*8; i++) {
-  board.appendChild(document.createElement("DIV"));
+const button = document.querySelector("button");
+
+button.addEventListener("click", newBoard())
+
+function newBoard() {
+  let input;
+
+  do {
+    input = Number(prompt("Enter a board size", 8));
+  } while(input === NaN)
+
+  generateBoard(input);
 }
 
-for(var i = 0; i < board.children.length; i++) {
-  board.children[i].addEventListener("mouseenter", e => e.target.style.backgroundColor = "red");
+  function generateBoard(size) {
+    for(var i = 0; i < size ** 2; i++) {
+    board.appendChild(document.createElement("DIV"));
+  }
+  addListeners();
 }
 
-function changeColor(node) {
-  node
+function addListeners() {
+    for(var i = 0; i < board.children.length; i++) {
+    board.children[i].addEventListener("mouseenter", e => e.target.style.backgroundColor = "red");
+  }
 }
